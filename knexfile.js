83cf86ba3d962ad0ff38c5bdd8d7f8590512c1fe
Kevin Mas <kevin.mas.sge@mango.com>
2020-03-24 13:44:38 +0100

    Bootstrap

---
+++ knexfile.js
@@ -0,0 +1,21 @@
+module.exports = {
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './.dev/database.sqlite3'
+    },
+    useNullAsDefault: true
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'MIGRATIONS'
+    }
+  }
+};
