5c283959193941279a222cfe14b82fe0f3405006
Kevin Mas <kevin.mas.sge@mango.com>
2020-03-25 14:53:02 +0100

    Send change metrics to statsd

--- src/sinks/statsd/index.js
+++ src/sinks/statsd/index.js
@@ -8,6 +8,10 @@ class StatsDSink {
     deploymentHappened(deployment) {
         this.client.increment(`fkm.deploymentfrequency.${deployment.tenant}.${deployment.deployable}.deployment`, 1, 1, [ deployment.tenant, deployment.deployable ])
     }
+
+    changeDeployed(tenant, deployable, change, duration) {
+        this.client.gauge(`fkm.leadtimeforchange.${tenant}.${deployable}.change`, duration, 1, [ tenant, deployable ])
+    }
 }
 
 module.exports = setup => new StatsDSink(setup)
--- test/sinks/statsd/index.test.js
+++ test/sinks/statsd/index.test.js
@@ -1,18 +1,23 @@
 const faker = require('faker')
 const Deployment = require('../../../src/metrics/deploymentFrequency/model/deployment')
+const Change = require('../../../src/metrics/leadTimeForChanges/model/change')
 const StatsDSink = require('../../../src/sinks/statsd')
 
 describe('StatsD Sink', () => {
     const TENANT = faker.random.uuid()
     const DEPLOYABLE = faker.random.uuid()
+    const COMMIT_ID = faker.random.uuid()
+    const DURATION = faker.random.number(0, 1000)
     const TIMESTAMP = +(new Date())
 
     const client = {
-        increment: jest.fn()
+        increment: jest.fn(),
+        gauge: jest.fn()
     }
 
     beforeEach(() => {
         client.increment.mockReset()
+        client.gauge.mockReset()
     })
 
     const sink = () => StatsDSink({ client })
@@ -23,5 +28,13 @@ describe('StatsD Sink', () => {
         currentSink.deploymentHappened(deployment)
 
         expect(client.increment).toHaveBeenCalledWith(`fkm.deploymentfrequency.${TENANT}.${DEPLOYABLE}.deployment`, 1, 1, [ TENANT, DEPLOYABLE ])
+    })
+
+    it('should emit a change', () => {
+        const currentSink = sink()
+        const change = new Change(COMMIT_ID, TIMESTAMP)
+        currentSink.changeDeployed(TENANT, DEPLOYABLE, change, DURATION)
+        
+        expect(client.gauge).toHaveBeenCalledWith(`fkm.leadtimeforchange.${TENANT}.${DEPLOYABLE}.change`, DURATION, 1, [ TENANT, DEPLOYABLE ])
     })
 })
