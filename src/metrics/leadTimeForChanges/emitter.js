c568ef237f8d39fb0b916d44485451f02b68cc9c
Kevin Mas <kevin.mas.sge@mango.com>
2020-03-26 11:07:43 +0100

    Sort ascending changes

--- src/metrics/leadTimeForChanges/emitter.js
+++ src/metrics/leadTimeForChanges/emitter.js
@@ -2,7 +2,7 @@ const logger = require('../../infrastructure/logger')
 
 module.exports = sink => changeSet => {
     const deploymentHappened = changeSet.happened;
-    const changes = changeSet.changes.sort((a, b) => b.happened - a.happened);
+    const changes = changeSet.changes.sort((a, b) => a.happened - b.happened);
 
     changes.forEach(change => {
         logger.info('changeDeployed', { ...change, duration: deploymentHappened - change.happened })
